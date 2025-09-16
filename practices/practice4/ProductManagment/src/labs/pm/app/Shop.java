dd503341d4e7f2f04e260569e5a98a1a7480237d
Housliman <hamza.naitousliman.work.00@gmail.com>
2025-09-16 04:39:50 +0100

    Now We are Talking

--- practices/practice4/ProductManagment/src/labs/pm/app/Shop.java
+++ practices/practice4/ProductManagment/src/labs/pm/app/Shop.java
@@ -7,11 +7,17 @@ import labs.pm.data.Product;
 public class Shop {
   public static void main(String[] args) {
     Product p1 = new Product();
-    p1.setId(101);
-    p1.setName("Tea");
+    // p1.setId(101);
+    // p1.setName("Tea");
     p1.setPrice(BigDecimal.valueOf(1.99));
 
-    System.out.println(p1.getId()+" "+p1.getName()+" "+p1.getPrice());
+    System.out.println(p1.getId()+" "+p1.getName()+" "+p1.getPrice()+" "+p1.getDiscount());
   }
-  
 }
+
+/**
+  * for runing this code use this steps:
+  ** Go outside of folder src 
+  ** run command `javac -d out $(find src -name "*.java")`
+  ** And then run `java -cp out labs.pm.app.Shop`
+*/
--- practices/practice4/ProductManagment/src/labs/pm/data/Product.java
+++ practices/practice4/ProductManagment/src/labs/pm/data/Product.java
@@ -1,19 +1,22 @@
 package labs.pm.data;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Product {
   private int id;
   private String name;
   private BigDecimal price;
+  public static final BigDecimal DISCOUNT_RATE=BigDecimal.valueOf(0.1);
 
-  public void setId(int id) {
+  public void setId(final int id) {
     this.id = id;
   }
-  public void setName(String name) {
+  public void setName(final String name) {
     this.name = name;
   }
-  public void setPrice(BigDecimal price) {
+  public void setPrice(final BigDecimal price) {
+    // price = BigDecimal.ONE;
     this.price = price;
   }
 
@@ -25,6 +28,10 @@ public class Product {
   }
   public BigDecimal getPrice() {
     return price;
+  }
+
+  public BigDecimal getDiscount() {
+    return price.multiply(DISCOUNT_RATE).setScale(2, RoundingMode.HALF_UP);
   }
   
 }
