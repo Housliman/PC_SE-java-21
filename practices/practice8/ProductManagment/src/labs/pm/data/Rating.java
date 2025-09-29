d9b97b7ed4185c407ccd777d8261ce385f944173
Housliman <hamza.naitousliman.work.00@gmail.com>
2025-09-29 10:46:44 +0100

    Add some emojis

--- practices/practice8/ProductManagment/src/labs/pm/data/Rating.java
+++ practices/practice8/ProductManagment/src/labs/pm/data/Rating.java
@@ -1,12 +1,18 @@
 package labs.pm.data;
 
 public enum Rating {
-  NOT_RATED("\u2606\u2606\u2606\u2606\u2606"),
-  ONE_STAR("\u2605\u2606\u2606\u2606\u2606"),
-  TWO_STAR("\u2605\u2605\u2606\u2606\u2606"),
-  THREE_STAR("\u2605\u2605\u2605\u2606\u2606"),
-  FOUR_STAR("\u2605\u2605\u2605\u2605\u2606"),
-  FIVE_STAR("\u2605\u2605\u2605\u2605\u2605");
+  NOT_RATED("🌑🌑🌑🌑🌑"),
+  ONE_STAR("⭐🌑🌑🌑🌑"),
+  TWO_STAR("⭐⭐🌑🌑🌑"),
+  THREE_STAR("⭐⭐⭐🌑🌑"),
+  FOUR_STAR("⭐⭐⭐⭐🌑"),
+  FIVE_STAR("⭐⭐⭐⭐⭐");
+  // NOT_RATED("\u2606\u2606\u2606\u2606\u2606"),
+  // ONE_STAR("\u2605\u2606\u2606\u2606\u2606"),
+  // TWO_STAR("\u2605\u2605\u2606\u2606\u2606"),
+  // THREE_STAR("\u2605\u2605\u2605\u2606\u2606"),
+  // FOUR_STAR("\u2605\u2605\u2605\u2605\u2606"),
+  // FIVE_STAR("\u2605\u2605\u2605\u2605\u2605");
 
   private String stars;
 
